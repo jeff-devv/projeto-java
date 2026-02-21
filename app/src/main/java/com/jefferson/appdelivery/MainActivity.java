5d23526bf0c406f8d4c2ce57bc05d6c3190f5b98
Xavees <luisfernando1337x@gmail.com>
2026-02-20 22:23:18 -0300

    FINALIZADO PRIMEIRA PARTE

---
+++ app/src/main/java/com/jefferson/appdelivery/MainActivity.java
@@ -0,0 +1,47 @@
+package com.jefferson.appdelivery;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText email, senha;
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        initComponents();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Loginactivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void initComponents() {
+        email = findViewById(R.id.edt_email);
+        senha = findViewById(R.id.edt_senha);
+        login = findViewById(R.id.btn_login);
+    }
+}
